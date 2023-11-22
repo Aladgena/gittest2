2bb38754976cfea822af2be32a8c959efa33b7e0
Aladgena <aladgena@gmail.com>
2023-11-22 09:41:34 +0300

    commit 3

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const server = express();
 
-server.all('/', (req, res)=>{
-	res.setHeader('Content-Type', 'text/html');
-	res.write('<link href = "https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed"; font-size: 22px;}</style> <p>Hosting Active</p>');
-	res.end();
+server.all('/', (req, res) => {
+  Data = new Date();
+  Hour = Data.getHours();
+  Minutes = Data.getMinutes();
+  Seconds = Data.getSeconds();
+  console.log("Just got a request!" + Hour + ":" + Minutes + ":" + Seconds)
+  res.send('Hosting Active')
 })
 function keepAlive() {
 	server.listen(3000, () => {console.log("Server is online!")});
